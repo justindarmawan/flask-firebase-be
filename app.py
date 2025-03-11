6a3092e8a30d747801d995d34f3cb9ff563cde28
justin.darmawan <jdarmawan.jd@gmail.com>
2025-03-11 22:30:50 +0700

    Update homepage API

--- app.py
+++ app.py
@@ -14,24 +14,6 @@ db = firestore.client()
 @app.route("/")
 def home():
     return jsonify({"message": "Flask Firebase API is running!"})
-
-@app.route("/add", methods=["POST"])
-def add_data():
-    try:
-        data = request.json
-        db.collection("test").document().set(data)
-        return jsonify({"success": True}), 200
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-@app.route("/list", methods=["GET"])
-def list_data():
-    try:
-        docs = db.collection("test").stream()
-        data = [doc.to_dict() for doc in docs]
-        return jsonify(data), 200
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
     
 @app.route("/homepage", methods=["GET"])
 def fetch_homepage():
